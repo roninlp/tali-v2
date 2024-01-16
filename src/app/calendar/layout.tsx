56bbbaa2f4b0c4646960f65a89b6380de491053e
ronin <ronin.lp@gmail.com>
2024-01-16 23:36:28 +0330

    feat: updated home page and minor change

---
+++ src/app/calendar/layout.tsx
@@ -0,0 +1,7 @@
+import { type ReactNode } from "react";
+
+const CalendarLayout = ({ children }: { children: ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default CalendarLayout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import { CreatePost } from "@/app/_components/create-post";
+import { buttonVariants } from "@/components/ui/button";
 import { getServerAuthSession } from "@/server/auth";
 import { api } from "@/trpc/server";
 import { ModeToggle } from "./_components/theme-toggle";
@@ -10,75 +10,35 @@ export default async function Home() {
   const session = await getServerAuthSession();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <ModeToggle />
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+    <main className="flex min-h-screen flex-col items-center bg-background text-foreground">
+      <div className="flex w-full justify-between px-16 py-8">
+        <div className="flex items-center justify-center gap-4">
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
+            href={session ? "/api/auth/signout" : "/api/auth/signin"}
+            className={buttonVariants({ variant: "outline" })}
           >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
+            {session ? "Sign out" : "Sign in"}
           </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
+          <p className="text-center text-foreground">
+            {session && <span>Logged in as {session.user?.name}</span>}
           </p>
-
-          <div className="flex flex-col items-center justify-center gap-4">
-            <p className="text-center text-2xl text-white">
-              {session && <span>Logged in as {session.user?.name}</span>}
-            </p>
-            <Link
-              href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-            >
-              {session ? "Sign out" : "Sign in"}
-            </Link>
-          </div>
         </div>
+        <ModeToggle />
+      </div>
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-7xl font-extrabold tracking-tight sm:text-[5rem]">
+          <span className="bg-gradient-to-b from-[#5c6cf7] to-[#0522c7] bg-clip-text text-transparent">
+            TaLi
+          </span>{" "}
+          App
+        </h1>
 
-        <CrudShowcase />
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-2xl text-foreground">
+            Project Management and Planning
+          </p>
+        </div>
       </div>
     </main>
-  );
-}
-
-async function CrudShowcase() {
-  const session = await getServerAuthSession();
-  if (!session?.user) return null;
-
-  const latestPost = await api.post.getLatest.query();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
   );
 }
