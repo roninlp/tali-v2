3941abd8df473c50ccff12438bc722391f8aab54
ronin <ronin.lp@gmail.com>
2024-01-27 00:40:42 +0330

    fix: type issue in test component

--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -34,9 +34,8 @@ export function CreatePost() {
       <button
         type="submit"
         className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-        disabled={createPost.isLoading}
       >
-        {createPost.isLoading ? "Submitting..." : "Submit"}
+        Submit
       </button>
     </form>
   );
